e7df8a6f2853ac536bc070cb8b34b8cf1cc6e5e3
helloalpaca <jms393497@gmail.com>
2023-07-19 13:40:51 +0900

    🎉Init🎉

---
+++ src/main/java/team/hanaro/hanamate/HanamateServerApplication.java
@@ -0,0 +1,13 @@
+package team.hanaro.hanamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanamateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanamateServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/hanaro/hanamate/HanamateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.hanaro.hanamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanamateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
