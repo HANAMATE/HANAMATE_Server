553ffc0436e8887be82e07b7ff7d1bbf66a0de7d
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-08-01 16:47:17 +0900

    Feat: 회원가입, 로그인, Redis 코드 구현

--- src/main/java/team/hanaro/hanamate/domain/User/Controller/UsersController.java
+++ src/main/java/team/hanaro/hanamate/domain/User/Controller/UsersController.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.hanaro.hanamate.domain.User.Helper;
 import team.hanaro.hanamate.domain.User.Dto.Response;
 import team.hanaro.hanamate.domain.User.Dto.UserRequestDto;
@@ -35,7 +32,7 @@ public class UsersController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Validated UserRequestDto.Login login, Errors errors) {
+    public ResponseEntity<?> login(@Validated @RequestBody UserRequestDto.Login login, Errors errors) {
         // validation check
         if (errors.hasErrors()) {
             return response.invalidFields(Helper.refineErrors(errors));
