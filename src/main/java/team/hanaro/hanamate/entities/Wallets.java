245aa8a171ede29228d7b8169ae01ba7b65157f3
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-08-01 17:04:51 +0900

    Fix: Wallets boolean 자료형 Boolean으로 변경

--- src/main/java/team/hanaro/hanamate/entities/Wallets.java
+++ src/main/java/team/hanaro/hanamate/entities/Wallets.java
@@ -15,9 +15,7 @@ public class Wallets {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long walletId;
-
-//    @Enumerated(EnumType.STRING)
-    private boolean walletType; /* 0: 개인, 1: 모임통장 */
+    private Boolean walletType; /* 0: 개인, 1: 모임통장 */
     private Long balance;
     private Long targetAmount; /* 모임통장일 때 목표금액 */
 }
