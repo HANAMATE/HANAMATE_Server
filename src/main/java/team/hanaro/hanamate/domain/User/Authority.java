a217923b0c8fabb9d7627e18fff24866c0c85fab
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-08-02 13:32:51 +0900

    Feat: 로그인, 회원가입 구현 (#4)
    * Feat: 로그인 페이지 구현 중, MySQL 연결
    
    * Feat: 로그인, 회원가입 예시 코드 작성
    
    * Feat: Entity 클래스에서는 Setter 사용 지양 -> @Builder로 수정
    
    * Feat: 경로 API 명세서에 맞게 수정
    
    * Feat: REST API 코드 추가 (상태 코드, 메시지 추가)
    
    * Feat: REST API 코드 추가 (상태 코드, 메시지 추가)
    
    * Feat: REST API 코드 다른 방식 (key, value 값으로 전송해야지 결과 나옴)
    
    * Feat: REST API 관련 코드 추가 (Entity, DTO 등 로직에 맞게 구현했는지 확인은 해봐야함)
    
    * Feat: Member에 Login 로직 합침
    
    * Feat: Member에 Login 로직 분리
    
    * Feat: Login, Signup 코드 Member로부터 분리하여 구현 -> 실제 DB와 맞게 코드 수정해야 함. 세션 코드 수정 및 비밃번호 보안 코드 추가하기, 예외처리 코드 작성하기
    
    * Feat: 실제 DB에 맞춰서 코드 수정 -> json에서 null값으로 들어가는 것들 추가하기
    
    * Feat: JWT, SpringSecurity 반영 -> 학습 후 수정해야함.
    
    * Comment: JWT 주석 추가
    
    * Feat: JWT Token 테스트 코드 작성 중
    
    * Feat: JWT 권한 구분
    
    * Feat: JWT Refresh 토큰 발급 중
    
    * Feat: LoginRes, Req DTO 구분지음
    
    * Feat: 모든 파일 주석 처리
    
    * Feat: 모든 파일 삭제 후 새로운 프로젝트 예시 코드 작성함.
    
    * Feat: 새로운 코드 오류 제거
    
    * Feat: 파일 이동
    
    * Feat: 회원가입, 로그인, Redis 작동하는 거 확인함
    
    * Feat: 회원가입, 로그인, Redis 코드 구현
    
    * Remove: 테스트 코드로 사용했던 HTML파일 및 상태 코드 파일 삭제
    
    * Fix: Wallets boolean 자료형 Boolean으로 변경
    
    * Fix: entites 파일 이동

---
+++ src/main/java/team/hanaro/hanamate/domain/User/Authority.java
@@ -0,0 +1,6 @@
+package team.hanaro.hanamate.domain.User;
+
+public enum Authority {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/team/hanaro/hanamate/domain/User/Service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package team.hanaro.hanamate.domain.User.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team.hanaro.hanamate.entities.Users;
+import team.hanaro.hanamate.domain.User.Repository.UsersRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    //DataBase에 접근하여 사용자 정보를 가져온다.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usersRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(Users users) {
+        return new User(users.getUsername(), users.getPassword(), users.getAuthorities());
+    }
+}
