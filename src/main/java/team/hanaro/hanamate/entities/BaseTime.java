990faf1b0346a9d96625ecb96bdcfa6cef6073e4
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-08-03 17:55:36 +0900

    Comment: 주석 추가

--- src/main/java/team/hanaro/hanamate/entities/BaseTime.java
+++ src/main/java/team/hanaro/hanamate/entities/BaseTime.java
@@ -12,11 +12,11 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTime {
-
+public class BaseTim// Entity가 생성되어 저장될 때 시간이 자동 저장됩니다.
     @CreatedDate
     private LocalDateTime createDate;
 
+    // 조회한 Entity 값을 변경할 때 시간이 자동 저장됩니다.
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 }
