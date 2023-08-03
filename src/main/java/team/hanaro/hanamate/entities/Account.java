cb00a73a501ba2d9550ab7d41bae60ebb87bbc7e
helloalpaca <jms393497@gmail.com>
2023-08-03 13:41:51 +0900

    Fix: Account Table명 삭제

--- src/main/java/team/hanaro/hanamate/entities/Account.java
+++ src/main/java/team/hanaro/hanamate/entities/Account.java
@@ -4,11 +4,9 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "allowance_history")
 @Getter
 @ToString
 @AllArgsConstructor
