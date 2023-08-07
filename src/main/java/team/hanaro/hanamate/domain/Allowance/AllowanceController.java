59380d60176f4187fc0e04370111c86d2359fe7b
helloalpaca <jms393497@gmail.com>
2023-08-07 15:40:24 +0900

    Feat: ResponseEntity 적용

--- src/main/java/team/hanaro/hanamate/domain/Allowance/AllowanceController.java
+++ src/main/java/team/hanaro/hanamate/domain/Allowance/AllowanceController.java
@@ -3,9 +3,8 @@ package team.hanaro.hanamate.domain.Allowance;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 //@Transactional
 @Tag(name = "용돈", description = "용돈과 관련된 기능")
@@ -18,21 +17,21 @@ public class AllowanceController {
     /* 1. 아이 : 용돈 조르기 요청 조회하기 */
     @Operation(summary = "[아이] 용돈 조르기 요청 조회", description = "용돈 조르기 한 내역 가져오기", tags = {"용돈"})
     @GetMapping("/request/list")
-    public List<AllowanceResponseDto.ChildResponseList> GetAllowanceList(@RequestBody AllowanceRequestDto.ChildRequestList childRequestList) {
+    public ResponseEntity<?> GetAllowanceList(@RequestBody AllowanceRequestDto.ChildRequestList childRequestList) {
         return allowanceService.getChildRequestList(childRequestList);
     }
 
     /* 2. 아이 : 용돈 조르기 요청 */
     @Operation(summary = "[아이] 용돈 조르기 요청", description = "용돈 조르기", tags = {"용돈"})
     @PostMapping("/request")
-    public String MakeAllowanceRequest(@RequestBody AllowanceRequestDto.ChildRequest childRequest) {
+    public ResponseEntity<?> MakeAllowanceRequest(@RequestBody AllowanceRequestDto.ChildRequest childRequest) {
         return allowanceService.makeChildRequest(childRequest);
     }
 
     /* 3. 부모 : 용돈 조르기 승인 */
     @Operation(summary = "[부모] 용돈 조르기 승인", description = "용돈 조르기 승인", tags = {"용돈"})
     @PostMapping("/request/approve")
-    public String ApproveAllowanceRequest(@RequestBody AllowanceRequestDto.ParentApprove parentApprove) {
+    public ResponseEntity<?> ApproveAllowanceRequest(@RequestBody AllowanceRequestDto.ParentApprove parentApprove) {
         return allowanceService.approveRequest(parentApprove);
     }
 
--- src/main/java/team/hanaro/hanamate/domain/Allowance/AllowanceService.java
+++ src/main/java/team/hanaro/hanamate/domain/Allowance/AllowanceService.java
@@ -1,6 +1,10 @@
 package team.hanaro.hanamate.domain.Allowance;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import team.hanaro.hanamate.domain.User.Dto.Response;
 import team.hanaro.hanamate.entities.Requests;
 
 import java.sql.Timestamp;
@@ -9,16 +13,14 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class AllowanceService {
 
     private final AllowanceRepository allowanceRepository;
+    private final Response response;
 
-    public AllowanceService(AllowanceRepository allowanceRepository) {
-        this.allowanceRepository = allowanceRepository;
-    }
-
-    public List<AllowanceResponseDto.ChildResponseList> getChildRequestList(AllowanceRequestDto.ChildRequestList childRequestList) {
+    public ResponseEntity<?> getChildRequestList(AllowanceRequestDto.ChildRequestList childRequestList) {
         System.out.println(childRequestList.getUserId());
         Optional<List<Requests>> myRequests = allowanceRepository.findAllByRequesterId(childRequestList.getUserId());
         if (myRequests.isPresent()) {
@@ -28,20 +30,20 @@ public class AllowanceService {
                 AllowanceResponseDto.ChildResponseList childResponseList = new AllowanceResponseDto.ChildResponseList(request);
                 childResponseListArrayList.add(childResponseList);
             }
-            return childResponseListArrayList;
+            return response.success(childResponseListArrayList, "용돈 조르기 요청 리스트 조회에 성공했습니다.", HttpStatus.OK);
         } else {
-            return null;
+            return response.fail("대기 상태의 용돈 조르기 요청이 없습니다.", HttpStatus.BAD_REQUEST);
         }
     }
 
-    public String makeChildRequest(AllowanceRequestDto.ChildRequest childRequest) {
+    public ResponseEntity<?> makeChildRequest(AllowanceRequestDto.ChildRequest childRequest) {
         Calendar cal = Calendar.getInstance();
         Timestamp requestDate = new Timestamp(cal.getTimeInMillis());
         cal.add(Calendar.DATE, 7);
         Timestamp expiredDate = new Timestamp(cal.getTimeInMillis());
 
         if (childRequest.getUserId() == null || childRequest.getAllowanceAmount() == null) {
-            return "failed";
+            return response.fail("용돈 조르기 요청에 실패했습니다.", HttpStatus.BAD_REQUEST);
         }
 
         Requests requests = Requests.builder()
@@ -54,22 +56,22 @@ public class AllowanceService {
                 .build();
         allowanceRepository.save(requests);
         allowanceRepository.flush();
-        return "success";
+        return response.success("용돈 조르기에 성공했습니다.");
     }
 
-    public String approveRequest(AllowanceRequestDto.ParentApprove parentApprove) {
+    public ResponseEntity<?> approveRequest(AllowanceRequestDto.ParentApprove parentApprove) {
         //1. 용돈 조르기 거부
         if (!parentApprove.getAskAllowance()) {
             int result = allowanceRepository.updateByRequestId(parentApprove.getRequestId(), parentApprove.getAskAllowance());
             if (result != -1) {
-                return "success";
+                return response.success("해당 용돈 조르기 요청을 거부했습니다.");
             } else {
-                return "failed";
+                return response.fail("용돈 조르기 요청 상태 변경에 실패했습니다.", HttpStatus.BAD_REQUEST);
             }
         }
         //2. 용돈 조르기 성공
         else {
-            return "개발 중";
+            return response.fail("개발중입니다.", HttpStatus.HTTP_VERSION_NOT_SUPPORTED);
         }
     }
 }
