87a76bb1a0f046dc023ef2b4d7aa1207470a6c7f
hELLOalpaca <33242017+helloalpaca@users.noreply.github.com>
2023-08-08 15:48:50 +0900

    Feat: 내 지갑 - 충전하기 (#12)
    * Fix: remove healthy check
    
    * Refactor: ResponseDto, RequestDto 수정
    
    * Feat: 은행 계좌 잔액 조회
    
    * Feat: 계좌 잔액이 부족할 때
    
    * 충전하기 WIP
    
    * Feat: 충전하기
    
    * Feat: 은행 계좌 연결하기
    
    * Fix: ResponseEntity 사용

--- src/main/java/team/hanaro/hanamate/domain/MyWallet/AccountRepository.java
+++ src/main/java/team/hanaro/hanamate/domain/MyWallet/AccountRepository.java
@@ -4,17 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-import team.hanaro.hanamate.entities.Wallets;
+import team.hanaro.hanamate.entities.Account;
 
 import java.util.Optional;
 
-public interface MyWalletRepository extends JpaRepository<Wallets, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @Override
-    Optional<Wallets> findById(Long aLong);
+    Optional<Account> findByMemberId(Long aLong);
 
     @Transactional
     @Modifying
-    @Query("update Wallets w set w.balance=:balance where w.walletId=:walletId")
-    int updateByWalletId(Long walletId, Long balance);
+    @Query("update Account a set a.balance=:balance where a.memberId=:memberId")
+    int updateByMemberId(Long memberId, Integer balance);
+
+
 }
