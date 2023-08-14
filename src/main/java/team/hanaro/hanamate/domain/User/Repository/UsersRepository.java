456781ab85600a837febddf030c7cbee9e575be5
helloalpaca <jms393497@gmail.com>
2023-08-14 23:11:02 +0900

    Fix: UserRepository 수정

--- src/main/java/team/hanaro/hanamate/domain/User/Repository/UsersRepository.java
+++ src/main/java/team/hanaro/hanamate/domain/User/Repository/UsersRepository.java
@@ -6,11 +6,11 @@ import team.hanaro.hanamate.entities.User;
 import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
-    Optional<User> findByIdx(Long aLong);
-
     //    Optional<Users> findByEmail(String email);
     Optional<User> findById(String id);
 
     //    boolean existsByEmail(String email);
     boolean existsById(String id);
+
+    Optional<User> findByIdx(Long aLong);
 }
