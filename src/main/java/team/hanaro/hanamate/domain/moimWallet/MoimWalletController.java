66b7f4a9c4f80efab55dc43eb3b13b1f2c872f79
helloalpaca <jms393497@gmail.com>
2023-08-18 18:24:32 +0900

    Fix: Swagger 설명 추가/수정

--- src/main/java/team/hanaro/hanamate/domain/moimWallet/MoimWalletController.java
+++ src/main/java/team/hanaro/hanamate/domain/moimWallet/MoimWalletController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 import team.hanaro.hanamate.domain.User.Helper;
 import team.hanaro.hanamate.domain.moimWallet.dto.MoimWalletRequestDto;
 import team.hanaro.hanamate.global.Response;
@@ -15,7 +16,7 @@ import team.hanaro.hanamate.global.Response;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@Tag(name = "모임 통장", description = "모임통장 컨트롤러")
+@Tag(name = "모임통장", description = "모임통장 컨트롤러")
 @RequiredArgsConstructor
 @RestController
 public class MoimWalletController {
@@ -23,6 +24,7 @@ public class MoimWalletController {
     private final MoimWalletService moimWalletService;
     private final Response response;
 
+    @ApiIgnore
     @GetMapping("/healthy")
     public ResponseEntity<?> HealthyCheck() {
         return response.success("moimWallet is healthy");
@@ -37,9 +39,9 @@ public class MoimWalletController {
         return moimWalletService.createMoimWallet(moimWallet);
     }
 
-    @GetMapping("/moim/{id}")
     @Operation(summary = "모임통장 내용 가져오기", description = "지정한 모임통장의 세부내역(거래내역, 글, 댓글) 등을 모두 가져옵니다.)", tags = {"모임통장"})
-    public ResponseEntity<?> getMoimWallet( @Validated @PathVariable("id")Long id) {
+    @GetMapping("/moim/{id}")
+        public ResponseEntity<?> getMoimWallet( @Validated @PathVariable("id")Long id) {
         return moimWalletService.getMoimWallet(id);
     }
 
@@ -53,6 +55,7 @@ public class MoimWalletController {
         }
         return moimWalletService.getMoimWalletListByLoginId(moimWallet);
     }
+    @Operation(summary = "모임 통장 삭제하기", description = "모임통장 삭제하기", tags = {"모임통장"})
     @DeleteMapping("/moim")
     public ResponseEntity<?> deleteMoimWallet(@Validated @RequestBody MoimWalletRequestDto.DeleteRequestDTO deleteRequestDTO, Errors errors) {
 
@@ -62,6 +65,7 @@ public class MoimWalletController {
         return moimWalletService.deleteMoimWallet(deleteRequestDTO);
     }
 
+    @Operation(summary = "거래내역 글쓰기", description = "거래내역 1개에 대해서 1개의 글을 쓸 수 있습니다.", tags = {"모임통장"})
     @PostMapping("/moim/article")
     public ResponseEntity<?> writeArticle(@Validated @RequestBody MoimWalletRequestDto.WriteArticleRequestDTO articleDTO, Errors errors){
         // validation check
@@ -80,6 +84,7 @@ public class MoimWalletController {
         }
     }
 
+    @Operation(summary = "거래내역 글 삭제하기", description = "거래내역 글을 삭제합니다.", tags = {"모임통장"})
     @DeleteMapping("/moim/article")
     public ResponseEntity<?> deleteArticle(@Validated @RequestBody MoimWalletRequestDto.DeleteRequestDTO deleteRequestDTO, Errors errors) {
 
