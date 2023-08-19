5056c9d2a629d3ffe594aae9e0b80b7434019668
petcu1004 <kty4585@naver.com>
2023-08-19 13:55:31 +0900

    Feat: Entity User 변경 이후 오류 코드 수정

--- src/main/java/team/hanaro/hanamate/domain/User/Repository/ChildRepository.java
+++ src/main/java/team/hanaro/hanamate/domain/User/Repository/ChildRepository.java
@@ -3,5 +3,9 @@ package team.hanaro.hanamate.domain.User.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.hanaro.hanamate.entities.Child;
 
+import java.util.Optional;
+
 public interface ChildRepository extends JpaRepository<Child,Long> {
+
+    Optional<Child> findByLoginId(String id);
 }
