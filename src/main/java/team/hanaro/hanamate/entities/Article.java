3bc43f92e49eda9f118ba45f3d7c98e9a8e18d7f
Ansix1207 <choiansik98@naver.com>
2023-08-21 17:04:36 +0900

    Feat: Article에 Cascade ALL옵션으로 변경

--- src/main/java/team/hanaro/hanamate/entities/Article.java
+++ src/main/java/team/hanaro/hanamate/entities/Article.java
@@ -36,7 +36,7 @@ public class Article extends BaseTime {
     private Transactions transaction;
 
     @Builder.Default
-    @OneToMany(mappedBy = "article",cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @OneToMany(mappedBy = "article",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
     //List<Commnets> comments = new ArrayList<>() 필드 초기화를 했으나
     //NPE(Nuill Pointer Exception)이 발생함. 디버깅 해보니 초기화가 안되어서 null 상태임
