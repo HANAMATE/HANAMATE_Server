c705a5228cfd97e6b68884725a7f44ef830d74c9
hELLOalpaca <33242017+helloalpaca@users.noreply.github.com>
2023-08-25 17:34:15 +0900

    Chore: Controller에 Swagger Operation 추가, SwaggerConfig수정 (#51)
    * Feat: Controller에 Swagger Operation 추가, Swagger 설정 정보 수정
    
    * fix: /parent-child Operation 정보 수정

--- src/main/java/team/hanaro/hanamate/domain/Loan/Controller/LoanController.java
+++ src/main/java/team/hanaro/hanamate/domain/Loan/Controller/LoanController.java
@@ -1,5 +1,7 @@
 package team.hanaro.hanamate.domain.Loan.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import team.hanaro.hanamate.domain.Loan.Service.LoanService;
 import team.hanaro.hanamate.domain.User.Helper;
 import team.hanaro.hanamate.global.Response;
 
+@Tag(name = "대출", description = "대출 기능")
 @Slf4j
 @RestController
 @RequestMapping("/loan")
@@ -22,17 +25,21 @@ public class LoanController {
     private final LoanService loanService;
     private final Response response;
 
+    @Operation(summary = "대출 정보 전달", description = "고정이자, 균등상환방식 사용자에게 정보 미리 전달", tags = {"대출"})
     //고정이자, 균등상환방식 사용자에게 정보 미리 전달
     @GetMapping("/applyForm")
-    public ResponseEntity<?> initLoanInfo(){
+    public ResponseEntity<?> initLoanInfo() {
         return loanService.initLoanInfo();
     }
 
+    @Operation(summary = "대출 정보 전달", description = "아이 - 대출 신청할떄 계산해줘서 보여주기 위함", tags = {"대출"})
     //아이 - 대출 신청할떄 계산해줘서 보여주기 위함
     @PostMapping("/calculate")
-    public ResponseEntity<?> calculate(@RequestBody LoanRequestDto.Calculate calculate, @AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> calculate(@RequestBody LoanRequestDto.Calculate calculate, @AuthenticationPrincipal UserDetails userDetails) {
         return loanService.calculate(calculate, userDetails.getUsername());
     }
+
+    @Operation(summary = "대출 등록", description = "대출 등록", tags = {"대출"})
     @PostMapping("/apply")
     public ResponseEntity<?> apply(@Validated @RequestBody LoanRequestDto.Apply apply, Errors errors, @AuthenticationPrincipal UserDetails userDetails) {
 //        log.info("대출 컨트롤러 들어옴");
@@ -45,36 +52,41 @@ public class LoanController {
     }
 
 
+    @Operation(summary = "대출 조회", description = "부모, 아이 - 대출 신청 정보 보기 (대출이름, 대출금액, 메세지)", tags = {"대출"})
     //부모, 아이 - 대출 신청 정보 보기 (대출이름, 대출금액, 메세지)
     @GetMapping("/applyInfo")
-    public ResponseEntity<?> applyInfo( @AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> applyInfo(@AuthenticationPrincipal UserDetails userDetails) {
         return loanService.applyInfo(userDetails.getUsername());
 
     }
 
 
+    @Operation(summary = "[부모] 대출 승인", description = "대출 승인 - 부모 (이때, 시작날짜, 마감날짜, 기간이 정해지고, valid는 true로 변경)", tags = {"대출"})
     //대출 승인 - 부모 (이때, 시작날짜, 마감날짜, 기간이 정해지고, valid는 true로 변경)
     @PostMapping("/approve")
-    public ResponseEntity<?> approve(@Validated @RequestBody LoanRequestDto.Approve approve, @AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> approve(@Validated @RequestBody LoanRequestDto.Approve approve, @AuthenticationPrincipal UserDetails userDetails) {
         return loanService.approve(approve, userDetails.getUsername());
     }
 
+    @Operation(summary = "[부모] 대출 거절", description = "대출 거절 => 삭제 - 부모 (대출 loans 테이블에서 삭제)", tags = {"대출"})
     //대출 거절 => 삭제 - 부모 (대출 loans 테이블에서 삭제)
     @PostMapping("/refuse")
-    public ResponseEntity<?> approve(@AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> approve(@AuthenticationPrincipal UserDetails userDetails) {
         return loanService.refuse(userDetails.getUsername());
     }
 
+    @Operation(summary = "대출 정보 가져오기", description = "대출 상품 기본 정보 가져오기(진행중일때) (대출이름, 대출금액, 마감날짜)", tags = {"대출"})
     //대출 상품 기본 정보 가져오기(진행중일때) (대출이름, 대출금액, 마감날짜)
     @GetMapping("/historyInfo")
-    public ResponseEntity<?> historyInfo(@AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> historyInfo(@AuthenticationPrincipal UserDetails userDetails) {
         return loanService.historyInfo(userDetails.getUsername());
     }
 
 
+    @Operation(summary = "대출 상세정보 가져오기", description = "대출 상품 상세 정보 가져오기 (회차, 거래날짜(=상환날짜), 상환금액, 상환 성공여부)", tags = {"대출"})
     //대출 상품 상세 정보 가져오기 (회차, 거래날짜(=상환날짜), 상환금액, 상환 성공여부)
     @GetMapping("/historydetailInfo")
-    public ResponseEntity<?> historydetailInfo(@AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<?> historydetailInfo(@AuthenticationPrincipal UserDetails userDetails) {
         return loanService.historydetailInfo(userDetails.getUsername());
     }
 
--- src/main/java/team/hanaro/hanamate/domain/User/Controller/ParentAndChildController.java
+++ src/main/java/team/hanaro/hanamate/domain/User/Controller/ParentAndChildController.java
@@ -1,39 +1,50 @@
 package team.hanaro.hanamate.domain.User.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import team.hanaro.hanamate.domain.User.Dto.UserRequestDto;
 import team.hanaro.hanamate.domain.User.Service.ParentAndChildService;
-import team.hanaro.hanamate.global.Response;
 
+@Tag(name = "부모아이관계", description = "부모-아이 관계 매핑 기능")
 @RestController
 @RequiredArgsConstructor
 public class ParentAndChildController {
     private final ParentAndChildService parentAndChildService;
 
+    @Operation(summary = "[부모] 아이 추가", description = "부모가 아이를 추가한다.", tags = {"부모아이관계"})
     @PostMapping("/parent/children")
     public ResponseEntity<?> addChild(@Validated @RequestBody UserRequestDto.ParentAddOrDeleteChildRequestDTO dto) {
         return parentAndChildService.addChild(dto);
     }
 
+    @Operation(summary = "[부모] 아이 찾기", description = "전화번호로 유저를 찾는다.", tags = {"부모아이관계"})
     @PostMapping("/parent/search")
     public ResponseEntity<?> findByUserPhoneNum(@Validated @RequestBody UserRequestDto.FindUserRequestDTO dto) {
         return parentAndChildService.findByUserPhoneNum(dto);
     }
 
+    @Operation(summary = "[부모] 내 아이 조회", description = "부모가 내 아이를 조회한다.", tags = {"부모아이관계"})
     @PostMapping("/parent/my-child")
     public ResponseEntity<?> findMyChild(@Validated @RequestBody UserRequestDto.getMyChildOrParentRequestDTO dto) {
         return parentAndChildService.getMyChildList(dto);
     }
+
+    @Operation(summary = "[아이] 내 부모 조회", description = "아이가 내 부모를 조회한다.", tags = {"부모아이관계"})
     @PostMapping("/child/my-parent")
     public ResponseEntity<?> findMyParent(@Validated @RequestBody UserRequestDto.getMyChildOrParentRequestDTO dto) {
         return parentAndChildService.getMyParentList(dto);
     }
 
+    @Operation(summary = "[All] 부모-아이 관계 삭제", description = "부모가 내 아이를 삭제한다.", tags = {"부모아이관계"})
     @DeleteMapping("/parent-child")
-    public ResponseEntity<?> deleteMapping(@Validated @RequestBody UserRequestDto.ParentAddOrDeleteChildRequestDTO dto){
+    public ResponseEntity<?> deleteMapping(@Validated @RequestBody UserRequestDto.ParentAddOrDeleteChildRequestDTO dto) {
         return parentAndChildService.deleteMyChild(dto);
     }
 }
