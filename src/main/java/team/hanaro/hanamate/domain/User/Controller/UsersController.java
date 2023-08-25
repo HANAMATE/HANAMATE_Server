e346e705b37753cfc7f26036f4446c42b4ac6a99
helloalpaca <jms393497@gmail.com>
2023-08-25 17:03:24 +0900

    Feat: Controller에 Swagger Operation 추가, Swagger 설정 정보 수정

--- src/main/java/team/hanaro/hanamate/domain/User/Controller/UsersController.java
+++ src/main/java/team/hanaro/hanamate/domain/User/Controller/UsersController.java
@@ -1,5 +1,7 @@
 package team.hanaro.hanamate.domain.User.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import team.hanaro.hanamate.jwt.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Tag(name = "인증인가", description = "인증/인가와 관련된 기능")
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -27,6 +30,7 @@ public class UsersController {
     private final UsersService usersService;
     private final Response response;
 
+    @Operation(summary = "회원가입", description = "회원가입", tags = {"인증인가"})
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@Validated @RequestBody UserRequestDto.SignUp signUp, Errors errors) {
         // validation check
@@ -36,6 +40,7 @@ public class UsersController {
         return usersService.signUp(signUp);
     }
 
+    @Operation(summary = "로그인", description = "로그인", tags = {"인증인가"})
     @PostMapping("/login")
     public ResponseEntity<?> login(@Validated @RequestBody UserRequestDto.Login login, Errors errors) {
         // validation check
@@ -45,9 +50,9 @@ public class UsersController {
         return usersService.login(login);
     }
 
-
+    @Operation(summary = "Init", description = "FE에서 첫 진입 시 AT토큰 유효 여부를 확인한다.", tags = {"인증인가"})
     @PostMapping("/init")
-    public ResponseEntity<?> init(@Validated UserRequestDto.Init init, Errors errors, HttpServletRequest request){
+    public ResponseEntity<?> init(@Validated UserRequestDto.Init init, Errors errors, HttpServletRequest request) {
         log.info("init 컨트롤러");
 
         if (errors.hasErrors()) {
@@ -57,6 +62,7 @@ public class UsersController {
     }
 
 
+    @Operation(summary = "AT 재발급", description = "RT로 AT를 재발급한다.", tags = {"인증인가"})
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(@Validated UserRequestDto.Reissue reissue, Errors errors, HttpServletRequest request) {
         // validation check
@@ -66,6 +72,7 @@ public class UsersController {
         return usersService.reissue(request);
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃", tags = {"인증인가"})
     @PostMapping("/logout")
     public ResponseEntity<?> logout(@Validated UserRequestDto.Logout logout, Errors errors) {
         // validation check
