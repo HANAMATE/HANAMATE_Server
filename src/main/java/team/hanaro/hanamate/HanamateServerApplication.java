6bb77a038c6f9eda6bdd18267aa0abf766996826
hELLOalpaca <33242017+helloalpaca@users.noreply.github.com>
2023-08-28 18:35:48 +0900

    Feat: 정기용돈 이체 스케줄러 (#28)
    * Feat: Scheduler 기능 구현 ing
    
    * Feat: 정기 용돈 이체 로직 작성
    
    * Feat: 대출 상환 로직 추가
    
    * Feat: 정기용돈 조회쿼리 수정
    
    * feat: 상환이 완료되었을때 loan 변경
    
    * hotfix

--- src/main/java/team/hanaro/hanamate/HanamateServerApplication.java
+++ src/main/java/team/hanaro/hanamate/HanamateServerApplication.java
@@ -3,9 +3,11 @@ package team.hanaro.hanamate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class HanamateServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/team/hanaro/hanamate/entities/LoanHistory.java
+++ src/main/java/team/hanaro/hanamate/entities/LoanHistory.java
@@ -18,10 +18,11 @@ public class LoanHistory {
     private Long historyId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="loanId")
+    @JoinColumn(name = "loanId")
     private Loans loans;
     private Integer sequence_time;
     private Timestamp transactionDate;
     private Integer repaymentAmount; //상환금액
+    @Setter
     private Boolean success;
 }
