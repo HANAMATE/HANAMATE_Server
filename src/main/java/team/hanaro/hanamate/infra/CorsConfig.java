a4d7a0e2899b191d5a81e29884cc6778fa78aad5
dlrghks2090 <dlrghks2090@gmail.com>
2023-10-30 01:15:44 +0900

    Feat: 포트없는 ec2 주소 추가

--- src/main/java/team/hanaro/hanamate/infra/CorsConfig.java
+++ src/main/java/team/hanaro/hanamate/infra/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all paths.
                 .allowedOrigins("http://localhost:3000", "http://localhost:80","http://localhost:3001", "http://hanamate-front.s3-website.ap-northeast-2.amazonaws.com", "https://front.hana-kdt.co.kr"
-                        ,"http://15.165.150.155:80", "http://15.165.150.155:3000")
+                        ,"http://15.165.150.155:80", "http://15.165.150.155:3000", "http://15.165.150.155")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "X-Refresh-Token") // 클라이언트에 노출할 헤더 설정
